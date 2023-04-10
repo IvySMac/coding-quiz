d963b1819d5edb117b46212dabdcac8455d9cd15
Creature Feature <ivy.mccurdy@gmail.com>
2023-04-09 20:34:27 -0500

    made question text size bigger and added a timer

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var startButton = document.getElementById("start-btn")
 var questionContainerElement = document.getElementById("question-container")
-
+var quizRules = document.getElementById("rules")
+var timerEl = document.getElementById('countdown');
 
 startButton.addEventListener('click',startQuiz)
 
+function countdown() {
+    var timeLeft = 100;
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent ="Timer: " + timeLeft; 
+  
+      if(timeLeft === 0) {
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
+
+ countdown();
 
 function startQuiz() {
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
-
+    quizRules.classList.add('hide')
 }
 
-function setNextQuestion() {
+//function setNextQuestion() {}
 
-}
-
-function selectAnswer() {
-
-}
+//function selectAnswer() {}
 
