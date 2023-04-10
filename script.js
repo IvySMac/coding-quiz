3b92d6b3c6a89a8cfc0df703e3a9591c58f59162
Creature Feature <ivy.mccurdy@gmail.com>
2023-04-09 19:13:48 -0500

    add javascript to hide and reveal the first question and set of answers

---
+++ script.js
@@ -0,0 +1,21 @@
+var startButton = document.getElementById("start-btn")
+var questionContainerElement = document.getElementById("question-container")
+
+
+startButton.addEventListener('click',startQuiz)
+
+
+function startQuiz() {
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
